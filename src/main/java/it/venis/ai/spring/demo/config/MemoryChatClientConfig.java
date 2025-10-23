b376671b2b3429e5ffb3a63523029ee331275df7
Simone Scannapieco <simone.scannapieco@realt.it>
2025-10-23 13:11:46 +0200

    Stub for default chat memory

--- src/main/java/it/venis/ai/spring/demo/config/MemoryChatClientConfig.java
+++ src/main/java/it/venis/ai/spring/demo/config/MemoryChatClientConfig.java
@@ -4,10 +4,16 @@ import java.util.List;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.BaseChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,15 +22,59 @@ import it.venis.ai.spring.demo.advisors.OllamaCostSavingsAdvisor;
 @Configuration
 public class MemoryChatClientConfig {
 
+    /*
+     * Done under-the-hood via Spring auto-configuration.
+     */
     @Bean
-    public ChatClient ollamaMemoryChatClient(OllamaChatModel ollamaChatModel, ChatMemory chatMemory) {
+    public ChatMemoryRepository chatMemoryRepository() {
+        return new InMemoryChatMemoryRepository();
+    }
 
-        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+    /*
+     * Custom bean for chat memory, based on the (auto-)configured chat memory repository.
+     * Done under-the-hood via Spring auto-configuration with maxMessages = 20.
+     */
+    @Bean
+    public ChatMemory chatMemory(ChatMemoryRepository chatMemoryRepository) {
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(chatMemoryRepository)
+                .maxMessages(10)
+                .build();
+    }
+
+    /*
+     * Custom bean for message chat memory advisor, based on the configured chat memory.
+     */
+    @Bean
+    public BaseChatMemoryAdvisor messageChatMemoryAdvisor(ChatMemory chatMemory) {
+        return MessageChatMemoryAdvisor.builder(chatMemory)
+                .order(1)
+                .build();
+    }
+
+    /*
+     * Custom bean for prompt chat memory advisor, based on the configured chat memory.
+     */
+    @Bean
+    public BaseChatMemoryAdvisor promptChatMemoryAdvisor(ChatMemory chatMemory) {
+        return PromptChatMemoryAdvisor.builder(chatMemory)
+                .order(1)
+                .build();
+    }
+
+    /*
+     * Custom chat client based on the configured chat advisor.
+     */
+    @Bean
+    public ChatClient ollamaMemoryChatClient(OllamaChatModel ollamaChatModel, ChatMemory chatMemory, @Qualifier("promptChatMemoryAdvisor") BaseChatMemoryAdvisor chatMemoryAdvisor) {
 
         ChatClient.Builder chatClientBuilder = ChatClient.builder(ollamaChatModel);
 
         return chatClientBuilder
-                .defaultAdvisors(List.of(new OllamaCostSavingsAdvisor(), memoryAdvisor))
+                .defaultAdvisors(List.of(
+                        new SimpleLoggerAdvisor(),
+                        new OllamaCostSavingsAdvisor(),
+                        chatMemoryAdvisor))
                 .defaultSystem(
                         """
                             Sei un assistente AI di nome LLamaMemoryBot, addestrato per intrattenere una
