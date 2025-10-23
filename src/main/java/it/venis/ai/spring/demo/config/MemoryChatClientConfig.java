633a9680ad4bd20bc8777ccdc6b7194f61f831a8
Simone Scannapieco <simone.scannapieco@realt.it>
2025-10-23 17:14:35 +0200

    Stub for default chat memory

--- src/main/java/it/venis/ai/spring/demo/config/MemoryChatClientConfig.java
+++ src/main/java/it/venis/ai/spring/demo/config/MemoryChatClientConfig.java
@@ -66,7 +66,7 @@ public class MemoryChatClientConfig {
      * Custom chat client based on the configured chat advisor.
      */
     @Bean
-    public ChatClient ollamaMemoryChatClient(OllamaChatModel ollamaChatModel, ChatMemory chatMemory, @Qualifier("promptChatMemoryAdvisor") BaseChatMemoryAdvisor chatMemoryAdvisor) {
+    public ChatClient ollamaMemoryChatClient(OllamaChatModel ollamaChatModel, ChatMemory chatMemory, @Qualifier("messageChatMemoryAdvisor") BaseChatMemoryAdvisor chatMemoryAdvisor) {
 
         ChatClient.Builder chatClientBuilder = ChatClient.builder(ollamaChatModel);
 
